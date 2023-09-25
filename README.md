eeb44064a061e2e184fa06813f1d2ad189b6f837
andrew.hale@dgs.virginia.gov <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>
2023-09-25 15:22:24 +0000

    Metrics Maker update and README update

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## Included Scripts
 
-|Scripts|Description|
-|:---   |:---       |
-|Isolate_report_builder.Rmd|R Markdown script to build a pdf report on a per isolate basis for the Ashe long read pipeline|
-|snp_tree_builder.R|R script used to create phylogenetic trees based on SNPs rather than evolutionary distance|
-|tree_builder.R|Base R script that snp_tree_builder is based off of|
-|metrics_maker.R|R script used to help automate the SARS-COV2 submission process|
+|Scripts|Description|Args|
+|:---   |:---       |:---|
+|Isolate_report_builder.Rmd|R Markdown script to build a pdf report on a per isolate basis for the Ashe long read pipeline||
+|snp_tree_builder.R|R script used to create phylogenetic trees based on SNPs rather than evolutionary distance||
+|tree_builder.R|Base R script that snp_tree_builder is based off of||
+|metrics_maker.R|R script used to help automate the SARS-COV2 submission process|Rscript metrics_maker.R Masterlist Start_ID End_ID monroe_summary output_filename|
--- metrics_maker.R
+++ metrics_maker.R
@@ -1,6 +1,7 @@
 library(readxl)
 library(writexl)
 library(dplyr)
+library(openxlsx)
 
 args <- commandArgs(trailingOnly = TRUE)
 data <- read_excel(args[1]) # nolint: line_length_linter.
@@ -15,6 +16,7 @@ metrics_frame <- metrics_frame %>% mutate(purpose =
  case_when(vax == "vaccinated" | ob != "NA" ~ "targetted_sequencing", # nolint: line_length_linter.
            vax == "vaccinated" & ob != "NA" ~ "targetted_sequencing")) # nolint: line_length_linter.
 metrics_frame["purpose"][is.na(metrics_frame["purpose"])] <- "baselinesurveillance" # nolint: line_length_linter.
+metrics_frame[, "date"] <- as.character(metrics_frame[["date"]])
 
 monroe_depths <- read.csv(args[4], header = TRUE)
 monroe_depths <- select(monroe_depths, c("sample", "mean_depth"))
@@ -24,12 +26,8 @@ monroe_depths <- subset(monroe_depths,
  sample >= args[2] & sample <= args[3])
 
 metrics_frame <- cbind(metrics_frame, monroe_depths$mean_depth)
-metrics_frame[, "date"] = as.character(metrics_frame[["date"]])
 
 names(metrics_frame) <- c("DCLS Sequencing ID", "DCLS Specimen ID", "Date of Collection", # nolint: line_length_linter.
  "Vaccination Status", "Outbreak ID", "Sequence Run Name", "Purpose of Sampling", "Mean Depth") # nolint: line_length_linter.
-
-print(monroe_depths)
-print(metrics_frame)
 
 write_xlsx(metrics_frame, args[5])
